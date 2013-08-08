0ecfc8e97c8bbb5615231f588e1d7980a84697ae
Nicolas ABRIC <nicolas.abric@gmail.com>
2013-08-08 22:57:24 +0200

    Bug FIx - Wrong object

--- client/src/common/filters/asset.js
+++ client/src/common/filters/asset.js
@@ -1,7 +1,7 @@
 angular.module('filters.xbmc.asset', [])
     .filter('asset', function () {
         return function (input, ip) {
-            if (input && conf) {
+            if (input && ip) {
                 return 'http://' + ip + ':8080/image/' + encodeURIComponent(input);
             }     else {
                 return '';
