5f7c2c55ed8b10e94b7a15d4b467bae17a4e2c61
Nicolas ABRIC <nicolas.abric@gmail.com>
2014-09-03 14:06:35 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	client/PSD/Aphelion.psd

--- client/src/common/directives/image.js
+++ client/src/common/directives/image.js
@@ -1,28 +1,41 @@
 "use strict";
 angular.module('directives.image', [])
-    .directive('source', function () {
-        return function (scope, elm, attrs) {
-            var element = elm[0];
-            var asChromeApp = false;
-            try {
-                asChromeApp = typeof window.localStorage === 'undefined';
-            } catch (e) {
-                asChromeApp = true;
-            }
-            if(asChromeApp) {
-                var remoteImage = new RAL.RemoteImage({
-                    element : element,
-                    src : attrs.source,
-                    width : element.offsetWidth
+    .directive('image', function () {
+        return  {
+            restrict: 'A',
+            scope: {
+                imageSource: '='
+            },
+            link: function (scope, elem, attrs) {
+                var element = elem[0];
+                var asChromeApp = false;
+                try {
+                    asChromeApp = typeof window.localStorage === 'undefined';
+                } catch (e) {
+                    asChromeApp = true;
+                }
+                var updateValue = function () {
+                    if(asChromeApp) {
+                        var remoteImage = new RAL.RemoteImage({
+                            element : element,
+                            src : scope.imageSource ,
+                            width : element.offsetWidth
+                        });
+                        RAL.Queue.add(remoteImage, true);
+                    } else {
+                        if(element.tagName.toLowerCase() === 'img') {
+                            element.src = scope.imageSource 
+                        } else {
+                            element.style.backgroundImage = 'url(' + scope.imageSource + ')';
+                        } 
+                        
+                    }
+                };
+                scope.$watch('imageSource', function (newVal, oldVal) {
+                    if (newVal) {
+                        updateValue();
+                    }
                 });
-                RAL.Queue.add(remoteImage, true);
-            } else {
-                if(element.tagName.toLowerCase() === 'img') {
-                    element.src = attrs.source;
-                } else {
-                    element.style.backgroundImage = 'url(' + attrs.source + ')';
-                } 
-                
             }
         };
     });
