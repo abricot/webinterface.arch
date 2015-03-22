71e37220907f5c76163fdb32aa25350c66d4717e
Nicolas ABRIC <nicolas.abric@gmail.com>
2015-03-22 13:52:13 +0100

    Recently added tv shows

---
+++ client/src/app/tvshow/list.js
@@ -0,0 +1,27 @@
+angular.module('app')
+.config(['$stateProvider',
+  function($stateProvider) {
+    $stateProvider.state('tvshows', {
+      url: '/tvshows',
+      views: {
+        header: {templateUrl: 'layout/headers/navigation.tpl.html', controller : 'HeaderNavController'},
+        body: { templateUrl: 'tvshow/list.tpl.html', controller: 'TvShowListCtrl'}
+      }
+    }).state('tvshows.all', { 
+      url : '/all',
+      templateUrl: 'tvshow/shows.tpl.html',
+      controller: 'ShowsCtrl'
+    }).state('tvshows.recents', { 
+      url: '/recents',
+      templateUrl: 'tvshow/episodes.tpl.html',
+      controller: 'EpisodesCtrl'
+    });
+  }
+])
+.controller('TvShowListCtrl', ['$scope',
+  function TvShowListCtrl($scope) {
+    $scope.isSelected = function (stateName) {
+      return $scope.$state.current.name === stateName;
+    }
+  }
+]);
