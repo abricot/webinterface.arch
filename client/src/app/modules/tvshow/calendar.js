db18777210bc7ddb8c62982a19d45f276a755165
Nicolas ABRIC <nicolas.abric@gmail.com>
2015-08-06 07:40:23 +0200

    Trakt - My calendar tv show

--- client/src/app/modules/tvshow/calendar.js
+++ client/src/app/modules/tvshow/calendar.js
@@ -1,24 +1,34 @@
 angular.module('app')
-.controller('ShowsCalendarCtrl', ['$scope', '$filter',
-  function ShowsCalendarCtrl($scope, $filter) {
+.controller('ShowsCalendarCtrl', ['$scope', '$filter', '$interpolate', '$anchorScroll',
+  function ShowsCalendarCtrl($scope, $filter, $interpolate, $anchorScroll) {
+    var playFn = $interpolate('http://{{ip}}:{{port}}/jsonrpc?request={ "jsonrpc": "2.0", "method": "Player.Open", "params" : {"item": { "file": "{{path}}" }}, "id": {{uid}}}');
+    
     $scope.loading = true;
-
-    var beginOfMonth = moment().startOf('month');
-    var current = beginOfMonth.month();
+    $scope.tvshows = [];
+    var iterator = moment().startOf('month');
+    iterator.subtract(iterator.day(), 'd');
+    var dates = [];
     $scope.dates = [];
+    while(iterator.month() <= moment().month()) {
+      dates.push(moment(iterator));
+      iterator.add(1, 'd');
+    }
 
-    while(current === beginOfMonth.month()) {
-      $scope.dates.push(moment(beginOfMonth));
-      beginOfMonth.add(1, 'd');
+    while(iterator.day() !== 0) {
+      iterator.add(1, 'd');
+      dates.push(moment(iterator));
     }
 
     function onTvShowsFromSource(result) {
       $scope.tvshows = result.data;
+      $scope.dates = dates;
       $scope.loading = false;
+      $anchorScroll('today');
     };
 
     function onLoad() {
-      $scope.trakt.calendar.myShows(moment().startOf('month').subtract(1, 'd').toDate(), 31).then(onTvShowsFromSource);
+      var days = dates[dates.length-1].diff(dates[0], 'days');
+      $scope.trakt.calendar.myShows(dates[0].toDate(), days).then(onTvShowsFromSource);
     };
 
     if ($scope.trakt.isAuthenticated()) {
@@ -26,6 +36,14 @@ angular.module('app')
     } else {
       $scope.go('/settings');
     }
+
+    $scope.isFuture = function(date){
+      return date.month()> moment().month();
+    };
+
+    $scope.isPast = function(date){
+      return date.month()< moment().month();
+    };
 
     $scope.isToday = function(date){
       var now = moment();
@@ -41,6 +59,28 @@ angular.module('app')
         var airDate = moment(show.first_aired);
         return date.isSame(airDate, 'day');
       });
+    };
+
+    $scope.play = function(episode){
+      if($scope.host.videoAddon.toLowerCase().indexOf('youtube') > -1) {  
+        $scope.tmdb.tv.videos(
+          episode.ids.tvdb, 
+          episode.season, 
+          episode.number).then(function(result){
+            var videos = result.data.results;
+            var pluginURL = 'plugin://'+$scope.host.videoAddon+'/?action=play_video&videoid='+videos[0].key;
+            $scope.xbmc.open({file: pluginURL});
+        });
+      } else {
+        var path = '/show/'+episode.ids.tvdb+'/season/'+episode.season+'/episode/'+episode.number+'/play';
+        var url = playFn({
+          ip : $scope.host.ip,
+          port : $scope.host.httpPort,
+          path : 'plugin://plugin.video.pulsar' + path,
+          uid : Date.now()
+        })
+        $http.get(url);
+      }
     };
   }
 ]);
