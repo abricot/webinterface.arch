a4ff50d52af0e8ada1b0f5fc8984365346217670
Nicolas ABRIC <nicolas.abric@gmail.com>
2015-08-27 20:33:59 +0200

    Better fanart animation, Month view calendar

--- client/src/app/modules/tvshow/calendar.js
+++ client/src/app/modules/tvshow/calendar.js
@@ -3,18 +3,17 @@ angular.module('app')
   function ShowsCalendarCtrl($scope, $filter, $interpolate, $anchorScroll, $http) {
     var playFn = $interpolate('http://{{ip}}:{{port}}/jsonrpc?request={ "jsonrpc": "2.0", "method": "Player.Open", "params" : {"item": { "file": "{{path}}" }}, "id": {{uid}}}');
     var autoscroll = true;
-    $scope.loading = true;
     $scope.fetching = false;
     $scope.tvshows = [];
-    var iterator = moment().startOf('month');
-    iterator.subtract(iterator.day(), 'd');
-    var dates = getDates(iterator, moment());
-    $scope.dates = [];
+    $scope.refDate = moment();
     $scope.tvshows = [];
+    $scope.shows = {};
 
     function getDates(date, ref) {
+      console.log(date.format('YYYY-MM-DD'),ref.format('YYYY-MM-DD'));
       var dates = [];
-      while(date.month() <= ref.month()) {
+      var days = ref.diff(date, 'days');
+      for(var i=0; i< days; i++){
         dates.push(moment(date));
         date.add(1, 'd');
       }
@@ -28,22 +27,27 @@ angular.module('app')
 
     function onTvShowsFromSource(result) {
       var tvshows = result ? result.data : [];
-      $scope.tvshows = $scope.tvshows.concat(tvshows);
-      $scope.dates = $scope.dates.concat(dates);
-      $scope.loading = false;
+      $scope.tvshows = tvshows;
+      $scope.shows = $scope.getShows();
+      $scope.dates = dates;
       $scope.fetching = false;
     };
 
-    function onLoad() {
+    function load(date, ref) {
+      dates = getDates(date, ref);
       var days = dates[dates.length-1].diff(dates[0], 'days');
       $scope.trakt.calendar.myShows(dates[0].toDate(), days).then(onTvShowsFromSource);
     };
 
     if ($scope.trakt.isAuthenticated()) {
-      onLoad();
+      var iterator = moment().startOf('month');
+      iterator.subtract(iterator.day(), 'd');
+      load(iterator, moment().startOf('month').add(1, 'month'));
     } else {
       $scope.trakt.connect().then(function(){
-        onLoad();
+        var iterator = moment().startOf('month');
+        iterator.subtract(iterator.day(), 'd');
+        load(iterator, moment().startOf('month').add(1, 'month'));
       }, function() {
         $scope.go('/settings');
       });
@@ -53,14 +57,35 @@ angular.module('app')
       return show.episode.images.screenshot.thumb || show.show.images.fanart.thumb;
     };
 
+    $scope.getBanner = function (show) {
+      return show.images.banner.full || show.images.thumb.full;
+    };
+
     $scope.getRandomImage = function () {
       return Math.floor(Math.random()*100)%11 + 1;
     }
 
-    $scope.getTVShowsFor = function(date) {
+    $scope.getShows = function () {
+      var shows = {};
+      $scope.tvshows.forEach(function(tvshow){
+        var showIds = tvshow.show.ids;
+        if(!shows.hasOwnProperty(showIds.trakt)){
+          shows[showIds.trakt] = angular.copy(tvshow.show);
+        }
+        shows[showIds.trakt].hit = shows[showIds.trakt].hit ? shows[showIds.trakt].hit+1 : 1;
+      });
+      return shows;
+    };
+
+    $scope.showsCount = function () {
+      return Object.keys($scope.shows).length;
+    }
+
+    $scope.getEpisodesFor = function(date, criteria) {
+      criteria = criteria || 'day';
       return $scope.tvshows.filter(function(show){
         var airDate = moment(show.first_aired);
-        return date.isSame(airDate, 'day');
+        return date.isSame(airDate, criteria);
       });
     };
 
@@ -77,18 +102,29 @@ angular.module('app')
       return date.isSame(now, 'day');
     };
 
-    $scope.loadMore = function () {
+    $scope.play = function(show){
+      $scope.helper.foreign.shows.play($scope.host, show.show, show.episode);
+    };
+
+    $scope.previousMonth = function () {
       if(!$scope.fetching) {
         $scope.fetching = true;
-        var last = $scope.dates[$scope.dates.length-1];
-        var date = moment(last).add(1, 'd');
-        dates = getDates(date, moment(last).add(1, 'month'));
-        onLoad();
+        var startOfMonth = $scope.refDate.startOf('month');
+        var date = moment(startOfMonth).subtract(1, 'd').startOf('month');
+        $scope.refDate = moment(date);
+        date.subtract(date.day(), 'd');
+        load(date, startOfMonth.subtract(1,'d'));
       }
     };
 
-    $scope.play = function(show){
-      $scope.helper.foreign.shows.play($scope.host, show.show, show.episode);
+    $scope.nextMonth = function() {
+      if(!$scope.fetching) {
+        $scope.fetching = true;
+        var startOfMonth = $scope.refDate.startOf('month').add(1, 'month');
+        $scope.refDate = moment(startOfMonth);
+        startOfMonth.subtract(startOfMonth.day(), 'd');
+        load(startOfMonth, $scope.refDate);
+      }
     };
 
     $scope.$on('onRepeatLast', function(scope, element, attrs){
